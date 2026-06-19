fe62de2f248cca2b9ee080d3aaa758a0b2b8021f
Jarno Parviainen <jarno.parviainen@gmail.com>
2026-06-19 13:20:23 +0300

    Add QSON text test corpus for invalid objects. Fix incorrect error during object key scanning when text ended unexpectedly.

--- packages/qson-js/src/parse.ts
+++ packages/qson-js/src/parse.ts
@@ -107,7 +107,8 @@ function parseString(state:ParserState):string {
 
 function parseKey(state:ParserState):string {
     const start = state.offset;
-    while (state.look !== ":") state.advance();
+    while (state.look && state.look !== ":") state.advance();
+    if (!state.look) throw new QSONSyntaxException(state, "Unexpected end of QSON input while scanning for object key");
     const end = state.offset;
     const encoded = state.slice(start, end);
     const decoded = decodeURIComponent(encoded);

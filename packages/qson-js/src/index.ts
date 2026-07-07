f9f9809f3149e5b5dce1ee0889449f825d3a4595
Jarno Parviainen <jarno.parviainen@gmail.com>
2026-07-07 17:42:27 +0300

    qson-js: Export most common types.

--- packages/qson-js/src/index.ts
+++ packages/qson-js/src/index.ts
@@ -2,3 +2,4 @@ export { default as stringifyQSON, type SerializerOptions } from "./stringify-qs
 export { default as parseQSON, type ParserOptions } from "./parse-qson.ts";
 export { default as createQueryString, type CreateQueryStringOptions } from "./create-query-string.ts";
 export { default as parseQueryString, type ParseQueryStringOptions } from "./parse-query-string.ts";
+export type { IsQSON, Obj, Transform } from "./types.ts";
